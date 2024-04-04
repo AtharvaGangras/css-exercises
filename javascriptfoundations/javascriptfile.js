5211c9c72920b73f414c2904bb2cb10020c06dcf
Atharva Gangras <acgangras@gmail.com>
2024-04-04 05:37:05 +0530

    create javascript foundation

---
+++ javascriptfoundations/javascriptfile.js
@@ -0,0 +1,10 @@
+console.log("this is from external javascript file")
+console.log(23+43)
+console.log(23+43+34+54+99+9340)
+console.log((4+6+9)/77)
+let a=10
+console.log(a)
+
+const MAX = 57
+let actual = MAX-13
+let percentage = actual/MAX
